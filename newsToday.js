6e8cdc26b65e02f7ead09e8c00c05cac4c4b36a8
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-03 03:18:26 +0100

    individual news category added

--- newsToday.js
+++ newsToday.js
@@ -10,12 +10,44 @@ const displayAllNewsCategories = categories => {
     console.log(categories)
     const newsCategoriesContainer = document.getElementById('newsCategories');
     categories.forEach(categorie => {
-        const singleCategorie = document.createElement('p');
+        const singleCategorie = document.createElement('small');
         singleCategorie.innerHTML = `
-        <div>${categorie.category_name}</div>
+        <div class="hoover p-1" onclick="loadIndividualCategory('${categorie.category_id}')">${categorie.category_name}</div>
     `
         newsCategoriesContainer.appendChild(singleCategorie);
     });
+}
 
+const loadIndividualCategory = (id) => {
+    console.log(id)
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayIndividualCategory(data.data))
+}
+const displayIndividualCategory = categories => {
+    console.log(categories)
+    const individualCategorieContainer = document.getElementById('individualCategorie');
+    individualCategorieContainer.innerHTML = '';
+    categories.forEach(categorie => {
+        console.log(categorie)
+        const individualCategorieDiv = document.createElement('div');
+        individualCategorieDiv.innerHTML = `
+        <div class="row">
+            <div class="border rounded mb-3 pt-2 pb-2 d-flex shadow">
+                <div class="col-md-3">
+                    <img src="${categorie.thumbnail_url}" class="img-fluid" alt="..">
+                </div>
+                <div class="col-md-9">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        individualCategorieContainer.appendChild(individualCategorieDiv);
+    })
 }
 loadAllNewsCategories()
