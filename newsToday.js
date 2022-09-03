0ff472a5e96ce8bafbd83a701cb97cc655e278d4
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-03 01:35:02 +0100

    categories added

--- newsToday.js
+++ newsToday.js
@@ -1,1 +1,21 @@
-console.log('hi')
+const loadAllNewsCategories = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayAllNewsCategories(data.data.news_category))
+        .catch(error => console.log(error))
+}
+
+const displayAllNewsCategories = categories => {
+    console.log(categories)
+    const newsCategoriesContainer = document.getElementById('newsCategories');
+    categories.forEach(categorie => {
+        const singleCategorie = document.createElement('p');
+        singleCategorie.innerHTML = `
+        <div>${categorie.category_name}</div>
+    `
+        newsCategoriesContainer.appendChild(singleCategorie);
+    });
+
+}
+loadAllNewsCategories()
