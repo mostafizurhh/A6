03512dbab6e820b8563dbca84b0f02f319595ee0
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-03 09:28:40 +0100

    spinner added

--- newsToday.js
+++ newsToday.js
@@ -1,3 +1,4 @@
+/******************************* display all news category ************************/
 const loadAllNewsCategories = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
@@ -11,12 +12,16 @@ const displayAllNewsCategories = categories => {
     const newsCategoriesContainer = document.getElementById('newsCategories');
     categories.forEach(categorie => {
         const singleCategorie = document.createElement('small');
+        /* start loader */
         singleCategorie.innerHTML = `
-        <div class="hoover p-1" onclick="loadIndividualCategory('${categorie.category_id}')">${categorie.category_name}</div>
+        <div class="hoover p-1" onclick="loadIndividualCategory('${categorie.category_id}'),toggleSpinner(true)">${categorie.category_name}</div>
     `
         newsCategoriesContainer.appendChild(singleCategorie);
     });
+
 }
+
+/******************** display individual category's all news *********************/
 
 const loadIndividualCategory = (id) => {
     console.log(id)
@@ -24,16 +29,18 @@ const loadIndividualCategory = (id) => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayIndividualCategory(data.data))
+        .catch(error => console.log(error))
 }
 const displayIndividualCategory = categories => {
     console.log(categories)
+    if (categories.length === 0) { alert('No data Available'); }
     const individualCategorieContainer = document.getElementById('individualCategorie');
     individualCategorieContainer.innerHTML = '';
     categories.forEach(categorie => {
         console.log(categorie)
         const individualCategorieDiv = document.createElement('div');
         individualCategorieDiv.innerHTML = `
-        <div class="row">
+        <div class="row" onclick="loadNewsDetail('${categorie._id}')">
             <div class="border rounded mb-3 pt-2 pb-2 d-flex shadow">
                 <div class="col-md-3 col-5">
                     <img src="${categorie.image_url}" class="img-fluid h-100 rounded" alt="..">
@@ -42,16 +49,15 @@ const displayIndividualCategory = categories => {
                     <div class="card-body">
                         <h5 class="card-title">${categorie.title}</h5>
                         <p class="card-text overflow">${categorie.details}</p> 
-                        <div class="bg-light mt-5 d-flex p-2  align-items-center justify-content-between">
-                            <div style="height: 70px;">
-                                <img class ="img-fluid mh-100 rounded-circle" src="${categorie.author.img}">
+                        <div class="mt-5 d-flex align-items-center justify-content-between justify-content-center">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <div>
+                                    <img  style="height: 70px;" class ="img-fluid mh-100 rounded-circle" src="${categorie.author.img}">
+                                    <p class="text-capitalize">${categorie.author.name ? categorie.author.name : 'Unknown'}</p>
+                                </div>    
                             </div>
                             <div>
-                                <p class="ps-2 m-0">${categorie.author.name ? categorie.author.name : 'No Author'}
-                                <p class="ps-2 m-0">${categorie.author.published_date ? categorie.author.published_date.slice(0, 10) : 'No Info'}
-                            </div>
-                            <div>
-                                <p>View: ${categorie.total_view ? categorie.total_view : 'No Data Available'}</p>
+                                <p>View: ${categorie.total_view ? categorie.total_view : 'No Views'}</p>
                             </div>
                         </div>  
                     </div>
@@ -61,6 +67,42 @@ const displayIndividualCategory = categories => {
         `
         individualCategorieContainer.appendChild(individualCategorieDiv);
     })
+    /* stop loader */
+    toggleSpinner(false);
 }
 
+/************************** display individual news details ************************/
+const loadNewsDetail = (_id) => {
+    console.log(_id)
+    const url = `https://openapi.programming-hero.com/api/news/${_id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsDetails(data.data))
+        .catch(error => console.log(error))
+}
+const displayNewsDetails = categories => {
+    console.log(categories)
+
+    categories.forEach(categorie => {
+        const modaltitle = document.getElementById('detailInfoModalLabel')
+        modaltitle.innerHTML = `${categorie.title}`
+        const modalBody = document.getElementById('modalDetail')
+        modalBody.innerHTML = `
+        <img class="img-fluid" src="${categorie.image_url}">
+        <p class="fw-bold p-1 text-capitalize">Author: ${categorie.author.name ? categorie.author.name : 'No Author'}</p>
+        <p class="mt-3">${categorie.details}</p>
+        `
+    })
+}
+
+// spinner function
+const toggleSpinner = isLoading => {
+    const spinner = document.getElementById('loader');
+    if (isLoading) {
+        spinner.classList.remove('d-none');
+    }
+    else {
+        spinner.classList.add('d-none');
+    }
+}
 loadAllNewsCategories()
